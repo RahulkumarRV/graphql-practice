7f53f4f5394f5bdc8c97cb5e4039ff3c68f0def6
= <rahulkumar@metafic.co>
2023-07-11 10:22:27 +0530

    update express graphql

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 const app = express();
 
-app.use('/graphql', expressGraphQL({
+app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
