41cdd729ffd885653ec23a94cca87a3c07f01eae
= <rahulkumar@metafic.co>
2023-07-15 15:38:29 +0530

    starting of graphql yoga

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-import express from 'express';
-import {graphqlHTTP} from 'express-graphql'
-import schema from './schema.js';
-import {books, authors} from './data.js';
-import { GraphQLObjectType, GraphQLString } from 'graphql';
-
-const app = express();
-
-const root = {
-  books: () => books,    // Add this resolver function to return all books
-  book: ({ title }) => books.find(book => book.title === title),
-  author: ({ name }) => authors.find(author => author.name === name),
-  createBook: ({ title, publicationYear, author }) => {
-    const book = { title, publicationYear, author };
-    books.push(book);
-    return book;
-  },
-  createAuthor: ({ name }) => {
-    const author = { name, books: [] };
-    authors.push(author);
-    return author;
+import { createSchema, createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
+export const schema = createSchema({
+  typeDefs: /* GraphQL */ `
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'world'
+    }
   }
-};
+})
 
-
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}));
-
-app.listen(3000, () => {
-  console.log("server listing on port 3000");
-});
+const yoga = createYoga({ schema })
+ 
+// Pass it into a server to hook into request handlers.
+const server = createServer(yoga)
+ 
+// Start the server and you're done!
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
